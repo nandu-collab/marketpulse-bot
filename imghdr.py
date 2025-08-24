987d37b914b3a5d138fc6d6b859f5615f4ffdb00
nandu-collab <nhegde636@gmail.com>
2025-08-24 15:57:18 +0530

    Create imghdr.py

---
+++ imghdr.py
@@ -0,0 +1,5 @@
+# Minimal shim for Python 3.13 where stdlib imghdr was removed
+def what(file, h=None):
+    # We don't need image detection for this bot (text-only posts)
+    # Returning None emulates "unknown type" and is safe.
+    return None
