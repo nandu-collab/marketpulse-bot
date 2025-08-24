cb618e536424ca04a8d7d411d6606cc637534dfc
nandu-collab <nhegde636@gmail.com>
2025-08-24 15:48:04 +0530

    Delete imghdr.py

--- imghdr.py
+++
@@ -1,6 +0,0 @@
-# Minimal shim for Python 3.13 where stdlib imghdr was removed
-def what(file, h=None):
-    # We don't need image detection for this bot (text-only posts)
-    # Returning None emulates "unknown type" and is safe.
-    return None
-    
